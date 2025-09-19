3cbd61bcc5e1c5a562df34fbccc5f1336528b03d
Poonam Dixit <poonamdxt06@gmail.com>
2025-09-19 18:32:38 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com', // yahan apni image domain dalni hai
+        pathname: '/**',
+      },
+      // agar aur domains hain, yahan add karo
+    ],
+  },
 };
 
 export default nextConfig;
